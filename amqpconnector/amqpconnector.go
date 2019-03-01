b7866cc7d453dd7eb7b6149871182960d63b963d
Vlad <@mail.ru>
2019-03-01 21:34:28 +0300

    Update amqp module

--- amqpconnector/amqpconnector.go
+++ amqpconnector/amqpconnector.go
@@ -1,3 +1,5 @@
+// Package amqpconnector is a wrapper for amqp package
+// with reconnect functional support
 package amqpconnector
 
 import (
@@ -10,7 +12,7 @@ import (
 //Consumer structure for NewConsumer result
 type Consumer struct {
 	conn       *amqp.Connection
-	channel    *amqp.Channel
+	Channel    *amqp.Channel
 	tag        string
 	Done       chan error
 	Deliveries <-chan amqp.Delivery
@@ -20,7 +22,7 @@ type Consumer struct {
 func NewConsumer(amqpURI, exchange, exchangeType, queueName, key, ctag string) (*Consumer, error) {
 	c := &Consumer{
 		conn:    nil,
-		channel: nil,
+		Channel: nil,
 		tag:     ctag,
 		Done:    make(chan error),
 	}
@@ -38,13 +40,13 @@ func NewConsumer(amqpURI, exchange, exchangeType, queueName, key, ctag string) (
 	}()
 
 	log.Printf("got Connection, getting Channel")
-	c.channel, err = c.conn.Channel()
+	c.Channel, err = c.conn.Channel()
 	if err != nil {
 		return nil, fmt.Errorf("Channel: %s", err)
 	}
 
 	log.Printf("got Channel, declaring Exchange (%q)", exchange)
-	if err = c.channel.ExchangeDeclare(
+	if err = c.Channel.ExchangeDeclare(
 		exchange,     // name of the exchange
 		exchangeType, // type
 		true,         // durable
@@ -57,7 +59,7 @@ func NewConsumer(amqpURI, exchange, exchangeType, queueName, key, ctag string) (
 	}
 
 	log.Printf("declared Exchange, declaring Queue %q", queueName)
-	queue, err := c.channel.QueueDeclare(
+	queue, err := c.Channel.QueueDeclare(
 		queueName, // name of the queue
 		true,      // durable
 		false,     // delete when unused
@@ -72,7 +74,7 @@ func NewConsumer(amqpURI, exchange, exchangeType, queueName, key, ctag string) (
 	log.Printf("declared Queue (%q %d messages, %d consumers), binding to Exchange (key %q)",
 		queue.Name, queue.Messages, queue.Consumers, key)
 
-	if err = c.channel.QueueBind(
+	if err = c.Channel.QueueBind(
 		queue.Name, // name of the queue
 		key,        // bindingKey
 		exchange,   // sourceExchange
@@ -83,7 +85,7 @@ func NewConsumer(amqpURI, exchange, exchangeType, queueName, key, ctag string) (
 	}
 
 	log.Printf("Queue bound to Exchange, starting Consume (consumer tag %q)", c.tag)
-	deliveries, err := c.channel.Consume(
+	deliveries, err := c.Channel.Consume(
 		queue.Name, // name
 		c.tag,      // consumerTag,
 		false,      // noAck
@@ -99,10 +101,55 @@ func NewConsumer(amqpURI, exchange, exchangeType, queueName, key, ctag string) (
 	return c, nil
 }
 
+func NewPublisher(amqpURI, exchange, exchangeType, key, ctag string) (*Consumer, error) {
+	c := &Consumer{
+		conn:    nil,
+		Channel: nil,
+		tag:     ctag,
+		Done:    make(chan error),
+	}
+
+	var err error
+
+	log.Printf("dialing %q", amqpURI)
+	c.conn, err = amqp.Dial(amqpURI)
+	if err != nil {
+		return nil, fmt.Errorf("Dial: %s", err)
+	}
+
+	go func() {
+		fmt.Printf("closing: %s", <-c.conn.NotifyClose(make(chan *amqp.Error)))
+	}()
+
+	log.Printf("got Connection, getting Channel")
+	c.Channel, err = c.conn.Channel()
+	if err != nil {
+		return nil, fmt.Errorf("Channel: %s", err)
+	}
+
+	log.Printf("got Channel, declaring Exchange (%q)", exchange)
+	if err = c.Channel.ExchangeDeclare(
+		exchange,     // name of the exchange
+		exchangeType, // type
+		true,         // durable
+		false,        // delete when complete
+		false,        // internal
+		false,        // noWait
+		nil,          // arguments
+	); err != nil {
+		return nil, fmt.Errorf("Exchange Declare: %s", err)
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("Queue Consume: %s", err)
+	}
+	return c, nil
+}
+
 //Shutdown channel on set app time tio live
 func (c *Consumer) Shutdown() error {
 	// will close() the deliveries channel
-	if err := c.channel.Cancel(c.tag, true); err != nil {
+	if err := c.Channel.Cancel(c.tag, true); err != nil {
 		return fmt.Errorf("Consumer cancel failed: %s", err)
 	}
 
@@ -114,84 +161,4 @@ func (c *Consumer) Shutdown() error {
 
 	// wait for handle() to exit
 	return <-c.Done
-}
-
-func Publish(amqpURI, exchange, exchangeType, routingKey, body string, reliable bool) error {
-
-	// This function dials, connects, declares, publishes, and tears down,
-	// all in one go. In a real service, you probably want to maintain a
-	// long-lived connection as state, and publish against that.
-
-	log.Printf("dialing %q", amqpURI)
-	connection, err := amqp.Dial(amqpURI)
-	if err != nil {
-		return fmt.Errorf("Dial: %s", err)
-	}
-	defer connection.Close()
-
-	log.Printf("got Connection, getting Channel")
-	channel, err := connection.Channel()
-	if err != nil {
-		return fmt.Errorf("Channel: %s", err)
-	}
-
-	log.Printf("got Channel, declaring %q Exchange (%q)", exchangeType, exchange)
-	if err := channel.ExchangeDeclare(
-		exchange,     // name
-		exchangeType, // type
-		true,         // durable
-		false,        // auto-deleted
-		false,        // internal
-		false,        // noWait
-		nil,          // arguments
-	); err != nil {
-		return fmt.Errorf("Exchange Declare: %s", err)
-	}
-
-	// Reliable publisher confirms require confirm.select support from the
-	// connection.
-	if reliable {
-		log.Printf("enabling publishing confirms.")
-		if err := channel.Confirm(false); err != nil {
-			return fmt.Errorf("Channel could not be put into confirm mode: %s", err)
-		}
-
-		confirms := channel.NotifyPublish(make(chan amqp.Confirmation, 1))
-
-		defer confirmOne(confirms)
-	}
-
-	log.Printf("declared Exchange, publishing %dB body (%q)", len(body), body)
-	if err = channel.Publish(
-		exchange,   // publish to an exchange
-		routingKey, // routing to 0 or more queues
-		false,      // mandatory
-		false,      // immediate
-		amqp.Publishing{
-			Headers:         amqp.Table{},
-			ContentType:     "text/plain",
-			ContentEncoding: "",
-			Body:            []byte(body),
-			DeliveryMode:    amqp.Transient, // 1=non-persistent, 2=persistent
-			Priority:        0,              // 0-9
-			// a bunch of application/implementation-specific fields
-		},
-	); err != nil {
-		return fmt.Errorf("Exchange Publish: %s", err)
-	}
-
-	return nil
-}
-
-// One would typically keep a channel of publishings, a sequence number, and a
-// set of unacknowledged sequence numbers and loop until the publishing channel
-// is closed.
-func confirmOne(confirms <-chan amqp.Confirmation) {
-	log.Printf("waiting for confirmation of one publishing")
-
-	if confirmed := <-confirms; confirmed.Ack {
-		log.Printf("confirmed delivery with delivery tag: %d", confirmed.DeliveryTag)
-	} else {
-		log.Printf("failed delivery of delivery tag: %d", confirmed.DeliveryTag)
-	}
 }
