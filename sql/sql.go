d4466ad7ddb52bde887a94aee0dec1050c6ef6d6
rider <zoobr@mail.ru>
2019-03-19 09:21:42 +0000

    Add SetStructValues method for sql

--- sql/sql.go
+++ sql/sql.go
@@ -1,7 +1,9 @@
 package sql
 
 import (
+	"fmt"
 	"os"
+	"reflect"
 	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -28,6 +30,74 @@ func SetValues(query *string, values *map[string]interface{}) error {
 	prepText += " "
 	strings.Replace(*query, "?", prepText, -1)
 	_, err := DB.NamedExec(*query, values)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//SetStructValues update helper with nodejs mysql style format
+//example UPDATE thing SET ? WHERE id = 123
+func SetStructValues(query *string, structVal interface{}) error {
+
+	val := reflect.ValueOf(structVal) // could be any underlying type
+
+	// if its a pointer, resolve its value
+	if val.Kind() == reflect.Ptr {
+		val = reflect.Indirect(val)
+	}
+
+	fields := reflect.TypeOf(val)
+	values := reflect.ValueOf(val)
+
+	num := fields.NumField()
+	resultMap := make(map[string]interface{})
+
+	prepText := " "
+	for i := 0; i < num; i++ {
+		field := fields.Field(i)
+		value := values.Field(i)
+		fmt.Print("Type:", field.Type, ",", field.Name, "=", value, "\n")
+
+		switch value.Kind() {
+		case reflect.String:
+			v := value.String()
+			resultMap[field.Name] = v
+		//int
+		case reflect.Int:
+			resultMap[field.Name] = value.Int()
+		case reflect.Int8:
+			resultMap[field.Name] = value.Int()
+		case reflect.Int16:
+			resultMap[field.Name] = value.Int()
+		case reflect.Int32:
+			resultMap[field.Name] = value.Int()
+		case reflect.Int64:
+			resultMap[field.Name] = value.Int()
+		//uint
+		case reflect.Uint8:
+			resultMap[field.Name] = value.Uint()
+		case reflect.Uint16:
+			resultMap[field.Name] = value.Uint()
+		case reflect.Uint32:
+			resultMap[field.Name] = value.Uint()
+		case reflect.Uint64:
+			resultMap[field.Name] = value.Uint()
+		//float
+		case reflect.Float32:
+			resultMap[field.Name] = value.Float()
+		case reflect.Float64:
+			resultMap[field.Name] = value.Float()
+		default:
+			fmt.Print("Not support type of struct")
+			continue
+		}
+		prepText += field.Name + "=:" + field.Name
+	}
+
+	prepText += " "
+	strings.Replace(*query, "?", prepText, -1)
+	_, err := DB.NamedExec(*query, &resultMap)
 	if err != nil {
 		return err
 	}
