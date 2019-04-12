fa90513745c9d9b358cd86996cc643ff28be0eab
rider <zoobr@mail.ru>
2019-04-12 15:32:05 +0300

    Fix timeout

--- terminalCommands/terminalCommands.go
+++ terminalCommands/terminalCommands.go
@@ -232,7 +232,7 @@ func (response *TerminalResponse) GetErrorsText() string {
 
 func NewSender(url string) Sender {
 	sender := Sender{
-		http:        &http.Client{Timeout: 30000},
+		http:        &http.Client{Timeout: 30000000},
 		terminalURL: &url,
 	}
 	return sender
