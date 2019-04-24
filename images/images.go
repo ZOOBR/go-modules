267ade588499dca1a396f9d1d2bf21edab17b9f3
Vlad <@mail.ru>
2019-04-24 21:51:07 +0300

    Added get thumbnail from exif

--- images/images.go
+++ images/images.go
@@ -13,6 +13,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/rwcarlsen/goexif/exif"
 	log "github.com/sirupsen/logrus"
 	str "gitlab.com/battler/modules/strings"
 )
@@ -23,11 +24,12 @@ type uploadedPhoto struct {
 }
 
 const (
-	S3_BUCKET         = "csx-docs"
-	S3_REGION         = "nl-ams"
-	S3_API_ACCESS_KEY = "SCWPT293A2FEE4NJZEW2"
-	S3_API_SECRET_KEY = "3c90bc78-e7d2-4b7a-a815-d64e3eaf7220"
-	S3_API_TOKEN      = "79bb5344-f2e9-4aa3-9c7a-9ad2f41ef9e5"
+	S3_BUCKET            = "csx-docs"
+	S3_BUCKET_THUMBNAILS = "csx-docs-tn"
+	S3_REGION            = "nl-ams"
+	S3_API_ACCESS_KEY    = "SCWPT293A2FEE4NJZEW2"
+	S3_API_SECRET_KEY    = "3c90bc78-e7d2-4b7a-a815-d64e3eaf7220"
+	S3_API_TOKEN         = "79bb5344-f2e9-4aa3-9c7a-9ad2f41ef9e5"
 )
 
 func UploadImage(photo *string, dir *string) (*uploadedPhoto, error) {
@@ -72,6 +74,19 @@ func UploadImageS3(photo *string, dir *string) (*uploadedPhoto, error) {
 		log.Error("Error decode photo: ", err)
 		return nil, err
 	}
+	var x *exif.Exif
+	x, err = exif.Decode(bytes.NewBuffer(dec))
+	if err != nil {
+		log.Error("Error decode exif: ", err)
+		return nil, err
+	}
+	var thumbnail []byte
+	thumbnail, err = x.JpegThumbnail()
+	if err != nil {
+		log.Error("Error gen thumbnail: ", err)
+		return nil, err
+	}
+
 	file := str.RandomString(10, false)
 	dest := strings.Join(strings.Split(file, ""), "/")
 	path := dest + "/" + file + ".jpg"
@@ -98,6 +113,22 @@ func UploadImageS3(photo *string, dir *string) (*uploadedPhoto, error) {
 	if err != nil {
 		log.Error("Error upload file to s3", err)
 		return nil, err
+	}
+
+	if len(thumbnail) > 0 {
+		_, err = s3.New(s).PutObject(&s3.PutObjectInput{
+			Bucket:             aws.String(S3_BUCKET_THUMBNAILS),
+			Key:                aws.String(path),
+			ACL:                aws.String("private"),
+			Body:               bytes.NewReader(thumbnail),
+			ContentLength:      aws.Int64(int64(len(thumbnail))),
+			ContentType:        aws.String(http.DetectContentType(thumbnail)),
+			ContentDisposition: aws.String("attachment"),
+		})
+		if err != nil {
+			log.Error("Error upload file to s3", err)
+			return nil, err
+		}
 	}
 
 	res := uploadedPhoto{
