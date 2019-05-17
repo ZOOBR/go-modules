af005bf8e1d67e3fe5c0cb3d49a2827801c6efc3
rider <zoobr@mail.ru>
2019-05-17 11:57:28 +0300

    Add s3 public constant

--- images/images.go
+++ images/images.go
@@ -34,6 +34,7 @@ const (
 	S3_CLIENT_DOCS       = "csx-docs"
 	S3_FINES             = "csx-fines"
 	S3_OBJECT_DAMAGES    = "csx-photo-damages"
+	S3_PUBLIC            = "csx-public"
 	S3_REGION            = "nl-ams"
 	S3_API_ACCESS_KEY    = "SCWPT293A2FEE4NJZEW2"
 	S3_API_SECRET_KEY    = "3c90bc78-e7d2-4b7a-a815-d64e3eaf7220"
@@ -44,6 +45,7 @@ var bucketsMap = map[string]string{
 	"docs":    S3_CLIENT_DOCS,
 	"fines":   S3_FINES,
 	"damages": S3_OBJECT_DAMAGES,
+	"public":  S3_PUBLIC,
 }
 
 func UploadImage(photo *string, dir *string) (*uploadedPhoto, error) {
