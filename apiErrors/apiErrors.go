c9fa4075765eb519e8346500dad5ed14b0055559
rider <zoobr@mail.ru>
2019-05-20 08:22:00 +0000

    Add apiErrors module

---
+++ apiErrors/apiErrors.go
@@ -0,0 +1,30 @@
+package apiErrors
+
+type ErrorItem struct {
+	statusCode int
+	messages   map[string]string
+}
+
+var (
+	errorsItems = map[string]ErrorItem{
+		"invalidPromocode": ErrorItem{
+			statusCode: 400,
+			messages: map[string]string{
+				"en": "Invalid promocode",
+				"ru": "Неправильный промокод",
+			},
+		},
+	}
+)
+
+func Error(errorCode string, lang string) (msg string, statusCode int) {
+	item, ok := errorsItems[errorCode]
+	if !ok {
+		return errorCode, 400
+	}
+	itemMsg, ok := item.messages[lang]
+	if !ok {
+		return errorCode, 400
+	}
+	return itemMsg, item.statusCode
+}
