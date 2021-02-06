deb3347a6b3688b2414c1e096b2d5967c32cce3f
Andrew Rider <zoobr@mail.ru>
2021-02-06 12:27:43 +0300

    Rework apiErrors models dependency removed

--- apiErrors/apiErrors.go
+++ apiErrors/apiErrors.go
@@ -1,11 +1,6 @@
 package apiErrors
 
-import (
-	"strings"
-
-	log "github.com/sirupsen/logrus"
-	"gitlab.com/battler/models"
-)
+import "strings"
 
 type ErrorItem struct {
 	statusCode int
@@ -16,20 +11,11 @@ var (
 	errorsItems = map[string]ErrorItem{}
 )
 
-func Init() {
-	langs, err := models.GetLangs()
-	if err != nil {
-		log.Error("Init locales err:", err)
-	}
-	for _, item := range langs {
-		errorsItems[item.Key] = ErrorItem{
-			statusCode: item.Status,
-			messages: map[string]string{
-				"en": item.En,
-				"ru": item.Ru,
-			},
-		}
-	}
+type Lang struct {
+	Key    string `db:"key" json:"key" key:"1"`
+	Status int    `db:"status" json:"status" type:"int4"`
+	Ru     string `db:"ru" json:"ru"`
+	En     string `db:"en" json:"en"`
 }
 
 func Error(errorCode string, lang string) (msg string, statusCode int) {
@@ -51,4 +37,16 @@ func Error(errorCode string, lang string) (msg string, statusCode int) {
 		return errorCode, 400
 	}
 	return itemMsg, item.statusCode
+}
+
+func Init(langs []Lang) {
+	for _, item := range langs {
+		errorsItems[item.Key] = ErrorItem{
+			statusCode: item.Status,
+			messages: map[string]string{
+				"en": item.En,
+				"ru": item.Ru,
+			},
+		}
+	}
 }
