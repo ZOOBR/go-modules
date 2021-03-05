0cfb97c787e33dfc05fd7876d7a761a91042a32a
Andrew Rider <zoobr@mail.ru>
2021-03-05 11:45:54 +0300

    Rename apiErrors to csxerrors Rework and refactoring Add new methods

--- csxerrors/csxerrors.go
+++ csxerrors/csxerrors.go
@@ -1,7 +1,12 @@
-package apiErrors
+package csxerrors
 
-import "strings"
+import (
+	"strings"
 
+	"gitlab.com/battler/modules/csxhttp"
+)
+
+// ErrorItem struct for send errors
 type ErrorItem struct {
 	statusCode int
 	messages   map[string]string
@@ -11,6 +16,7 @@ var (
 	errorsItems = map[string]ErrorItem{}
 )
 
+// Lang struct for load langs
 type Lang struct {
 	Key    string `db:"key" json:"key" key:"1"`
 	Status int    `db:"status" json:"status" type:"int4"`
@@ -18,6 +24,17 @@ type Lang struct {
 	En     string `db:"en" json:"en"`
 }
 
+// GetCtxLang return lang from request
+func GetCtxLang(ctx *csxhttp.Context) string {
+	lang := strings.Replace(ctx.Request().Header.Get("Accept-Language"), " ", "", -1)
+	parts := strings.Split(lang, ",")
+	if len(parts) > 1 {
+		lang = parts[1]
+	}
+	return lang
+}
+
+// Error return response message and status code by error ID
 func Error(errorCode string, lang string) (msg string, statusCode int) {
 	if lang == "" {
 		lang = "en"
@@ -39,6 +56,14 @@ func Error(errorCode string, lang string) (msg string, statusCode int) {
 	return itemMsg, item.statusCode
 }
 
+// Result get request lang and return result status, status code and translated message
+func Result(ctx *csxhttp.Context, errorCode string) (bool, int, string) {
+	lang := GetCtxLang(ctx)
+	msg, statusCode := Error(errorCode, lang)
+	return statusCode < 400, statusCode, msg
+}
+
+// Init load translates for responses
 func Init(langs []Lang) {
 	for _, item := range langs {
 		errorsItems[item.Key] = ErrorItem{
