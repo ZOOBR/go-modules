9c1ad819bec84d9b4b2f58fd84d3851c11f5d2c2
Vladimir Fetisov <v.fetisov@carusel.club>
2021-04-22 12:40:08 +0300

    added GenerateXLSXFromMap in reporter

--- reporter/reporter.go
+++ reporter/reporter.go
@@ -114,3 +114,82 @@ func GenerateXLSXFromTable(table *[][]interface{}, buf *bytes.Buffer) error {
 
 	return nil
 }
+
+// GenerateXLSXFromMap
+func GenerateXLSXFromMap(table []map[string]interface{}, buf *bytes.Buffer) error {
+	var err error
+
+	// Create output xlsx workbook
+	xfile := xlsx.NewFile()
+	xsheet, err := xfile.AddSheet("Sheet1")
+	if err != nil {
+		return fmt.Errorf("error adding sheet to xlsx file, %s\n", err)
+	}
+
+	var keys []string
+
+	//generate header from keys and save keys
+	xrow := xsheet.AddRow()
+	for k, _ := range table[0] {
+		xcell := xrow.AddCell()
+		xcell.SetString(k)
+		keys = append(keys, k)
+	}
+
+	// add rows
+	for i := 0; i < len(table); i++ {
+		xrow := xsheet.AddRow()
+		for _, k := range keys {
+			xcell := xrow.AddCell()
+			switch v := table[i][k].(type) {
+			case string:
+				xcell.SetString(v)
+			case []byte:
+				xcell.SetString(string(v))
+			case int64:
+				xcell.SetInt64(v)
+			case float64:
+				xcell.SetFloat(v)
+			case bool:
+				xcell.SetBool(v)
+			case time.Time:
+				xcell.SetDateTime(v)
+			case *string:
+				if v != nil {
+					xcell.SetString(*v)
+				} else {
+					xcell.SetString("")
+				}
+			case *float64:
+				if v != nil {
+					xcell.SetFloat(*v)
+				} else {
+					xcell.SetString("")
+				}
+			case *time.Time:
+				if v != nil {
+					xcell.SetDateTime(*v)
+				} else {
+					xcell.SetString("")
+				}
+			case *int:
+				if v != nil {
+					xcell.SetInt(*v)
+				} else {
+					xcell.SetString("")
+				}
+			default:
+				// todo: mybe add more types ?
+				xcell.SetString(fmt.Sprintf("%v", v))
+			}
+		}
+	}
+
+	// Save the excel file to the provided output file
+	err = xfile.Write(buf)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
