66440379416a39599e6f96bc10eace2c2c83ee5c
keyrano <dudorov.kirill@yandex.ru>
2021-04-26 06:40:39 +0300

    Rename module timers to 'csxtimers'

--- csxtimers/csxtimers.go
+++ csxtimers/csxtimers.go
@@ -1,10 +1,9 @@
-package timers
+package csxtimers
 
 import "time"
 
 // SetTimeout js style
 func SetTimeout(someFunc func(), milliseconds int) *time.Timer {
-
 	timeout := time.Duration(milliseconds) * time.Millisecond
 
 	// This spawns a goroutine and therefore does not block
@@ -14,7 +13,6 @@ func SetTimeout(someFunc func(), milliseconds int) *time.Timer {
 
 // SetInterval js style
 func SetInterval(someFunc func(), milliseconds int, async bool) chan bool {
-
 	// How often to fire the passed in function
 	// in milliseconds
 	interval := time.Duration(milliseconds) * time.Millisecond
@@ -28,7 +26,6 @@ func SetInterval(someFunc func(), milliseconds int, async bool) chan bool {
 	// so that the for loop is none blocking
 	go func() {
 		for {
-
 			select {
 			case <-ticker.C:
 				if async {
@@ -42,12 +39,10 @@ func SetInterval(someFunc func(), milliseconds int, async bool) chan bool {
 				ticker.Stop()
 				return
 			}
-
 		}
 	}()
 
 	// We return the channel so we can pass in
 	// a value to it to clear the interval
 	return clear
-
 }
