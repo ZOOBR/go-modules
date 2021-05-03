2affc9e04191953f89104f8b0106ab19349fb0c0
Andrew Rider <zoobr@mail.ru>
2021-05-03 18:48:46 +0300

    Add SetIntervalChan func to timers

--- timers/timers.go
+++ timers/timers.go
@@ -12,6 +12,46 @@ func SetTimeout(someFunc func(), milliseconds int) *time.Timer {
 
 }
 
+// SetIntervalChan js style
+func SetIntervalChan(someFunc func(chan bool), milliseconds int, async bool) chan bool {
+
+	// How often to fire the passed in function
+	// in milliseconds
+	interval := time.Duration(milliseconds) * time.Millisecond
+
+	// Setup the ticket and the channel to signal
+	// the ending of the interval
+	ticker := time.NewTicker(interval)
+	clear := make(chan bool)
+
+	// Put the selection in a go routine
+	// so that the for loop is none blocking
+	go func() {
+		for {
+
+			select {
+			case <-ticker.C:
+				if async {
+					// This won't block
+					go someFunc(clear)
+				} else {
+					// This will block
+					someFunc(clear)
+				}
+			case <-clear:
+				ticker.Stop()
+				return
+			}
+
+		}
+	}()
+
+	// We return the channel so we can pass in
+	// a value to it to clear the interval
+	return clear
+
+}
+
 // SetInterval js style
 func SetInterval(someFunc func(), milliseconds int, async bool) chan bool {
 
