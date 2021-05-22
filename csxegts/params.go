920f5b1d8bb545cc8952e04f1f861a9a9152324d
keyrano <dudorov.kirill@yandex.ru>
2021-05-22 20:10:47 +0300

    Add EGTS params

---
+++ csxegts/params.go
@@ -0,0 +1,11 @@
+package csxegts
+
+// ---------------------------------------------------------------------------------
+// Common
+// ---------------------------------------------------------------------------------
+
+// Version of the EGTS header structure
+const egtsHeaderProtocolVersion = byte(1) // 0x01
+
+// Prefix of the EGTS header for current version
+const egtsHeaderPrefix = "00"
