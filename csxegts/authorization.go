0202db9649d983996af84982adfe8cbbe3079244
keyrano <dudorov.kirill@yandex.ru>
2021-05-22 21:18:18 +0300

    Add creating authorization packet to module 'csxegts'

---
+++ csxegts/authorization.go
@@ -0,0 +1,28 @@
+package csxegts
+
+import "github.com/kuznetsovin/egts-protocol/app/egts"
+
+func newTermIdentityData(imei string) *egts.SrTermIdentity {
+	data := egts.SrTermIdentity{
+		TerminalIdentifier: 0,
+		MNE:                "0",
+		BSE:                "0",
+		NIDE:               "0",
+		SSRA:               "1",
+		LNGCE:              "0",
+		IMSIE:              "0",
+		IMEIE:              "1",
+		HDIDE:              "0",
+		IMEI:               imei,
+		// BufferSize
+	}
+
+	return &data
+}
+
+func CreateAuthPacket(objectIdentifier uint32, imei string) *Packet {
+	recordData := newTermIdentityData(imei)
+	authFrameData := newServiceFrameData(objectIdentifier, RpPriorityHigh, egts.AuthService, egts.SrTermIdentityType, recordData)
+
+	return newPacket(PacketIDCounter.Next(), egts.PtAppdataPacket, PacketPriorityHigh, authFrameData)
+}
