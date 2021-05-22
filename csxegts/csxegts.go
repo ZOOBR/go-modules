696c7c3ef5be883e6d6b58a3ca7d4b790153a9f2
keyrano <dudorov.kirill@yandex.ru>
2021-05-22 20:32:06 +0300

    Add creating new record to module 'csxegts'

--- csxegts/csxegts.go
+++ csxegts/csxegts.go
@@ -1,6 +1,20 @@
 package csxegts
 
+import (
+	"github.com/kuznetsovin/egts-protocol/app/egts"
+)
+
 var (
 	PacketIDCounter     = Counter{}
 	RecordNumberCounter = Counter{}
 )
+
+func newRecord(recordType byte, data egts.BinaryData) *egts.RecordData {
+	r := egts.RecordData{
+		SubrecordType:   recordType,
+		SubrecordLength: data.Length(),
+		SubrecordData:   data,
+	}
+
+	return &r
+}
