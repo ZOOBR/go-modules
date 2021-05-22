e5c60db3e88dc54f085898c320d0c381c0f958fd
keyrano <dudorov.kirill@yandex.ru>
2021-05-22 20:42:38 +0300

    Add source & recipient services position

--- csxegts/params.go
+++ csxegts/params.go
@@ -9,3 +9,23 @@ const egtsHeaderProtocolVersion = byte(1) // 0x01
 
 // Prefix of the EGTS header for current version
 const egtsHeaderPrefix = "00"
+
+// ---------------------------------------------------------------------------------
+// Source Service On Device
+// ---------------------------------------------------------------------------------
+
+// Source Service on terminal
+const SsodTerminal = "1"
+
+// Source Service on telematic platform
+const SsodPlatform = "0"
+
+// ---------------------------------------------------------------------------------
+// Recipient Service On Device
+// ---------------------------------------------------------------------------------
+
+// Recipient Service on terminal
+const RsodTerminal = "1"
+
+// Recipient Service on telematic platform
+const RsodPlatform = "0"
