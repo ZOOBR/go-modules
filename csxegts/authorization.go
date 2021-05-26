237e8d118b721a8ca2b5470d0d21236a9bb385a1
keyrano <dudorov.kirill@yandex.ru>
2021-05-26 21:30:26 +0300

    Comment & remove unused subrecords for auth packet in module 'csxegts'

--- csxegts/authorization.go
+++ csxegts/authorization.go
@@ -2,24 +2,6 @@ package csxegts
 
 import "github.com/kuznetsovin/egts-protocol/app/egts"
 
-// UNUSED NOW
-// func newTermIdentityData() *egts.SrTermIdentity {
-// 	data := egts.SrTermIdentity{
-// 		TerminalIdentifier: 0,
-// 		MNE:                "0",
-// 		BSE:                "0",
-// 		NIDE:               "0",
-// 		SSRA:               "1",
-// 		LNGCE:              "0",
-// 		IMSIE:              "0",
-// 		IMEIE:              "0",
-// 		HDIDE:              "0",
-// 		// BufferSize
-// 	}
-
-// 	return &data
-// }
-
 func newDispatcherIdentityData(dispatcherID uint32, description string) *SrDispatcherIdentity {
 	data := SrDispatcherIdentity{
 		DispatcherType: 0, // this field was not described in standart
@@ -53,7 +35,7 @@ func CreateAuthPacket(dispatcherID uint32, description string) (*Packet, uint16)
 
 func CreateServiceInfoPacket() (*Packet, uint16) {
 	recordData := []subrecordData{
-		{SubrecordType: SrServiceInfoType, SubrecordData: newServiceInfoData(egts.AuthService)},
+		// {SubrecordType: SrServiceInfoType, SubrecordData: newServiceInfoData(egts.AuthService)}, // not necessary
 		{SubrecordType: SrServiceInfoType, SubrecordData: newServiceInfoData(egts.TeledataService)},
 	}
 	authFrameData, recNum := newServiceFrameData(nil, RpPriorityHigh, egts.AuthService, recordData)
