34b46e421b92f473c864a788c7ec87ab3563c76c
keyrano <dudorov.kirill@yandex.ru>
2021-06-03 16:07:49 +0300

    Add Float64Eq() to module 'csxutils'

--- csxutils/maths.go
+++ csxutils/maths.go
@@ -2,6 +2,8 @@ package csxutils
 
 import "math"
 
+var epsilon = math.Nextafter(1, 2) - 1
+
 // ---------------------------------------------------------------------------------
 // Trigonometry
 // ---------------------------------------------------------------------------------
@@ -12,4 +14,13 @@ func DegToRad(degree float64) float64 {
 
 func RadToDeg(radian float64) float64 {
 	return radian * (180 / math.Pi)
+}
+
+// ---------------------------------------------------------------------------------
+// Precise comparisons on real numbers
+// ---------------------------------------------------------------------------------
+
+// Float64Eq returns true if two float64 numbers are equal
+func Float64Eq(f1, f2 float64) bool {
+	return math.Abs(f1-f2) < epsilon
 }
