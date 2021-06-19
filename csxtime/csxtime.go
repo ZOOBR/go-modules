db6fba347a925a306c6447fa9546b71a86bb6bb3
Roman <r.kudryashov@carusel.club>
2021-06-19 19:55:31 +0300

    added the functions of the beginning and end of the day and the format of string functions for the past dates

--- csxtime/csxtime.go
+++ csxtime/csxtime.go
@@ -24,6 +24,30 @@ func MonthBegin(timeCheck time.Time) time.Time {
 	return firstDay
 }
 
+// DayBegin return time begin of day
+func DayBegin(timeCheck time.Time) time.Time {
+	year, month, day := timeCheck.Date()
+	dayBegin := time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
+	return dayBegin
+}
+
+// DayEnd return time end of day
+func DayEnd(timeCheck time.Time) time.Time {
+	year, month, day := timeCheck.Date()
+	dayBegin := time.Date(year, month, day, 23, 59, 59, 1e9-1, time.UTC)
+	return dayBegin
+}
+
+// DayBeginStr return time begin of day to string
+func DayBeginStr(timeCheck time.Time) string {
+	return DayBegin(timeCheck).Format("2006-01-02 15:04:05.000000")
+}
+
+// DayEndStr return time begin of day to string
+func DayEndStr(timeCheck time.Time) string {
+	return DayEnd(timeCheck).Format("2006-01-02 15:04:05.000000")
+}
+
 func NowTimeToMilliseconds() float64 {
 	return float64(time.Now().UTC().UnixNano() / int64(time.Millisecond))
 }
