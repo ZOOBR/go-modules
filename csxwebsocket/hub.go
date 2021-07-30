c6151b5a409e07232d9bef2a0d75656d910f116f
Andrew Rider <zoobr@mail.ru>
2021-07-30 11:38:08 +0300

    Rework websocket hub Add mutex for clients Add base work methods

--- csxwebsocket/hub.go
+++ csxwebsocket/hub.go
@@ -1,6 +1,12 @@
 package csxwebsocket
 
-import "github.com/sirupsen/logrus"
+import (
+	"sync"
+
+	"github.com/sirupsen/logrus"
+)
+
+var clientsMutex sync.RWMutex
 
 // Hub maintains the set of active clients and broadcasts messages to the
 // clients.
@@ -21,6 +27,29 @@ type Hub struct {
 	Unregister chan *Client
 }
 
+// registerClient register new client
+func (h *Hub) registerClient(client *Client) {
+	clientsMutex.Lock()
+	h.clients[client.ID] = client
+	clientsMutex.Unlock()
+	logrus.Debug("register websocket client: ", client.ID)
+}
+
+// unregisterClient unregister client and force close connection
+func (h *Hub) unregisterClient(client *Client, useLock bool) {
+	if useLock {
+		clientsMutex.Lock()
+	}
+	if _, ok := h.clients[client.ID]; ok {
+		delete(h.clients, client.ID)
+		close(client.send)
+	}
+	if useLock {
+		clientsMutex.Unlock()
+	}
+	logrus.Debug("unregister websocket client: ", client.ID)
+}
+
 // NewHub constructor for create new hub
 func NewHub() *Hub {
 	return &Hub{
@@ -32,8 +61,38 @@ func NewHub() *Hub {
 	}
 }
 
-func (h *Hub) GetHubClients() map[string]*Client {
-	return h.clients
+// GetClientByID get client by ID
+func (h *Hub) GetClientByID(clientID string) (*Client, bool) {
+	clientsMutex.RLock()
+	client, ok := h.clients[clientID]
+	clientsMutex.RUnlock()
+	return client, ok
+}
+
+// RangeClients iterate all clients in hub and run callback
+func (h *Hub) RangeClients(cb func(client *Client)) {
+	clientsMutex.Lock()
+	for _, client := range h.clients {
+		cb(client)
+	}
+	clientsMutex.Unlock()
+}
+
+// SendBroadcast send broadcast with prepare message callback
+func (h *Hub) SendBroadcast(cb func(*Client) (*Message, bool)) {
+	clientsMutex.Lock()
+	for _, client := range h.clients {
+		message, ok := cb(client)
+		if !ok {
+			continue
+		}
+		select {
+		case client.send <- message:
+		default:
+			h.unregisterClient(client, false)
+		}
+	}
+	clientsMutex.Unlock()
 }
 
 // Run prepare hub channels
@@ -41,37 +100,32 @@ func (h *Hub) Run() {
 	for {
 		select {
 		case client := <-h.register:
-			logrus.Debug("register websocket client: ", client.ID)
-			h.clients[client.ID] = client
+			h.registerClient(client)
 		case client := <-h.Unregister:
-			logrus.Debug("unregister websocket client: ", client.ID)
-			if _, ok := h.clients[client.ID]; ok {
-				delete(h.clients, client.ID)
-				close(client.send)
-			}
+			h.unregisterClient(client, true)
 		case message := <-h.Send:
 			for i := 0; i < len(message.Clients); i++ {
 				clientID := message.Clients[i]
-				client := h.clients[clientID]
-				if client == nil {
+				client, ok := h.GetClientByID(clientID)
+				if !ok {
 					continue
 				}
 				select {
 				case client.send <- message:
 				default:
-					close(client.send)
-					delete(h.clients, clientID)
+					h.unregisterClient(client, true)
 				}
 			}
 		case message := <-h.Broadcast:
-			for clientID, client := range h.clients {
+			clientsMutex.Lock()
+			for _, client := range h.clients {
 				select {
 				case client.send <- message:
 				default:
-					close(client.send)
-					delete(h.clients, clientID)
+					h.unregisterClient(client, false)
 				}
 			}
+			clientsMutex.Unlock()
 		}
 	}
 }
